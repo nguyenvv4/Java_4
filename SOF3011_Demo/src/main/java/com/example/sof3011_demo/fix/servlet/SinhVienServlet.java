6d80558057b3f5665a641869678e7b2cda83d753
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-03-13 12:55:40 +0700

    update

--- SOF3011_Demo/src/main/java/com/example/sof3011_demo/fix/servlet/SinhVienServlet.java
+++ SOF3011_Demo/src/main/java/com/example/sof3011_demo/fix/servlet/SinhVienServlet.java
@@ -10,8 +10,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@WebServlet(name = "SinhVienController", value = {"/sinh-vien/hien-thi",
-        "/sinh-vien/add",
+@WebServlet(name = "SinhVienController", value = {"/sinh-vien/hien-thi" ,// GET
+        "/sinh-vien/add", // POST
         "/sinh-vien/detail"})
 public class SinhVienServlet extends HttpServlet {
     ArrayList<SinhVien> list = new ArrayList<>();
@@ -24,33 +24,20 @@ public class SinhVienServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String uri = request.getRequestURI();
-        if (uri.contains("hien-thi")) {
             ArrayList<SinhVien> list = hienthi();
             request.setAttribute("listSinhVien", list);
             request.getRequestDispatcher("/view/sinh-vien.jsp").forward(request, response);
-        } else if (uri.contains("detail")) {
-            Integer id = Integer.valueOf(request.getParameter("id"));
-            SinhVien sinhVien = detail(id);
-            request.setAttribute("sinhVien", sinhVien);
-            request.getRequestDispatcher("/view/detail.jsp").forward(request, response);
-        }
-
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String uri = request.getRequestURI();
-        if (uri.contains("/sinh-vien/add")) {
-            Integer id = Integer.valueOf(request.getParameter("name"));
-            String name = request.getParameter("name");
-            String address = request.getParameter("address");
-            Integer status = Integer.valueOf(request.getParameter("status"));
-            SinhVien sv = new SinhVien(id, name, address, status);
-            list.add(sv);
-            response.sendRedirect("/sinh-vien/hien-thi");
-
-        }
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        Integer status = Integer.parseInt(request.getParameter("status"));
+        SinhVien sinhVien = new SinhVien(id, name, address, status);
+        list.add(sinhVien);
+        response.sendRedirect("/sinh-vien/hien-thi");
     }
 
     private ArrayList<SinhVien> hienthi() {
